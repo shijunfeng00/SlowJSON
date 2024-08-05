89e1a9d481d257f84cfb9dd75974bb1c9da2d6f4
shijunfeng <1195885687@qq.com>
2024-08-05 17:23:25 +0800

    [commit] - 完善注释

--- slowjson/load.hpp
+++ slowjson/load.hpp
@@ -9,6 +9,12 @@
 #include "dynamic_dict.hpp"
 
 namespace slow_json {
+    /**
+     * 从字符串中加载JSON，并用其来反序列化对象
+     * @tparam T 被反序列化的对象类型
+     * @param value 被反序列化的对象
+     * @param json JSON字符串
+     */
     template<typename T>
     static void loads(T &value, const std::string &json) {
         slow_json::dynamic_dict dict(json);
