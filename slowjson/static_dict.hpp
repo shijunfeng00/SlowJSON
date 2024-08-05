e49123f00a8a0c097e4d3be45f774699421dba40
shijunfeng <1195885687@qq.com>
2024-08-05 17:18:37 +0800

    [fix] - 添加一个断言，用来发现类型错误

--- slowjson/static_dict.hpp
+++ slowjson/static_dict.hpp
@@ -9,14 +9,25 @@
 #include "dump_to_string_interface.hpp"
 
 namespace slow_json {
-    template<typename...Args>
+    /**
+     * 编译期静态字典
+     * @tparam Args 字典的简直对类型，为std::pair
+     */
+    template<typename ...Args>
     struct static_dict : public std::tuple<Args...> {
         using super_type = std::tuple<Args...>;
 
-        constexpr explicit static_dict(Args &&...args) : std::tuple<Args...>(std::forward<Args>(args)...) {}
+        constexpr explicit static_dict(Args &&...args) : std::tuple<Args...>(std::forward<Args>(args)...) {
+            assert_with_message((concepts::pair<Args> && ...), "static_dict元素非键值对(请参考函数签名)");
+        }
 
-        constexpr explicit static_dict(const Args &...args) : std::tuple<Args...>(args...) {}
+        constexpr explicit static_dict(const Args &...args) : std::tuple<Args...>(args...) {
+            assert_with_message((concepts::pair<Args> && ...), "static_dict元素非键值对(请参考函数签名)");
+        }
 
+        /**
+         * 简直对数量
+         */
         constexpr static std::size_t size_v = std::tuple_size_v<std::tuple<Args...>>;
 
         template<std::size_t index = 0, char...chs>
