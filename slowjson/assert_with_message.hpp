a392f14bd8aca66d5e17d6a07aa76da18712dab3
shijunfeng <1195885687@qq.com>
2024-08-23 14:47:16 +0800

    [fix] - 修改断言输出格式

--- slowjson/assert_with_message.hpp
+++ slowjson/assert_with_message.hpp
@@ -18,7 +18,7 @@
 #else
 #define assert_with_message(expr, message, ...) {                                                                                                                      \
        if(!(expr)){                                                                                                                                                  \
-           fprintf(stderr,"程序断言失败,程序被迫结束\n断言表达式:(%s)=%s\n文件:%s\n行数:%d\n函数完整签名:%s\n断言错误消息:",#expr,expr?"true":"false",__FILE__,__LINE__,__PRETTY_FUNCTION__);\
+           fprintf(stderr,"断言失败,程序退出\n断言表达式:(%s) 的值为 %s\n文件:%s\n行数:%d\n函数名称:%s\n断言错误消息:",#expr,expr?"true":"false",__FILE__,__LINE__,__PRETTY_FUNCTION__);\
            fprintf(stderr,message"\n",##__VA_ARGS__);                                                                                                                    \
            std::terminate();                                                                                                                                                  \
        }                                                                                                                                                             \
