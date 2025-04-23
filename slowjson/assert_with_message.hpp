20c1bf891b06a695b05746a6d4f385965da0c7e3
shijunfeng <1195885687@qq.com>
2025-04-23 12:45:58 +0800

    feature:测试单元的断言修改为异常 - 测试单元需要测试是否触发断言，但是断言本身不能捕获,因此测试阶段修改为异常

--- slowjson/assert_with_message.hpp
+++ slowjson/assert_with_message.hpp
@@ -16,6 +16,16 @@
 #ifdef NDEBUG
 #define assert_with_message(...)
 #else
+#ifdef BUILD_TEST_UNIT
+#define assert_with_message(expr, message, ...) {                                                                                                                      \
+       if(!(expr)){                                                                                                                                                  \
+           char str[500]={0};                                                                                                                                          \
+           memset(str,0,sizeof(str));\
+           snprintf(str,500,"断言失败,程序退出\n断言表达式:(%s) 的值为 %s\n文件:%s\n行数:%d\n函数名称:%s\n断言错误消息:"#message"\n",#expr,expr?"true":"false",__FILE__,__LINE__,__PRETTY_FUNCTION__,##__VA_ARGS__);           \
+           throw std::runtime_error(str);                                                                                                                                                  \
+       }                                                                                                                                                                 \
+}
+#else
 #define assert_with_message(expr, message, ...) {                                                                                                                      \
        if(!(expr)){                                                                                                                                                  \
            fprintf(stderr,"断言失败,程序退出\n断言表达式:(%s) 的值为 %s\n文件:%s\n行数:%d\n函数名称:%s\n断言错误消息:",#expr,expr?"true":"false",__FILE__,__LINE__,__PRETTY_FUNCTION__);\
@@ -23,5 +33,6 @@
            std::terminate();                                                                                                                                                  \
        }                                                                                                                                                             \
 }
+#endif
 #endif
 #endif //SLOWJSON_ASSERT_WITH_MESSAGE_HPP
