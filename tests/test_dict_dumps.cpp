8ec14f592944a55e4731a74f7d3a29b82646214c
shijunfeng <1195885687@qq.com>
2025-04-24 02:35:46 +0800

    test:完善测试用例

--- tests/test_dict_dumps.cpp
+++ tests/test_dict_dumps.cpp
@@ -33,4 +33,70 @@ void test_dict_dumps() {
     "name":"shijunfeng",
     "test":123
 })", "采用slow_json::dumps序列化得到的结果不正确");
+    slow_json::dict dict2{
+            {"name", std::string{"str3"}},
+            {"value", 123},
+            {"enabled", true},
+            {"list", {
+                             1,
+                             "str1",
+                             std::string{"str2"},
+                             3.4f,
+                             std::vector{11, 22, 33, 44},
+                             slow_json::dict{
+                                     {"a", 1234},
+                                     {"b", 456},
+                                     {"c", std::tuple<int, const char*>{2, "2"}},
+                                     {"list2", {1, "2", 3.456789}}
+                             },
+                             slow_json::list{1, 2, 3.456789}
+                     }},
+            {"nested_dict", {
+                             {"x", 1},
+                             {"y", "2.3asd"},
+                             {"z", 2.345f}
+                     }}
+    };
+    buffer.clear();
+    slow_json::dumps(buffer,dict2,4);
+    assert_with_message(buffer.string()==R"({
+    "nested_dict":{
+        "y":"2.3asd",
+        "z":2.345,
+        "x":1
+    },
+    "list":[
+        1,
+        "str1",
+        "str2",
+        3.4,
+        [
+            11,
+            22,
+            33,
+            44
+        ],
+        {
+            "list2":[
+                1,
+                "2",
+                3.456789
+            ],
+            "c":[
+                2,
+                "2"
+            ],
+            "b":456,
+            "a":1234
+        },
+        [
+            1,
+            2,
+            3.456789
+        ]
+    ],
+    "value":123,
+    "enabled":true,
+    "name":"str3"
+})","slow_json::dumps序列化结恶果不正确")
 }
